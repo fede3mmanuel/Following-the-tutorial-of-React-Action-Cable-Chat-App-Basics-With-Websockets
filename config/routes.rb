37e8a8db400d0f3f9a908f0bbef7fbc200839653
Emmanuel Sanchez <fede@fede.website>
2023-06-02 17:24:01 -0600

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # action cable_server
-  mount Action.server => "/cable"
+  mount ActionCable.server => "/cable"
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
