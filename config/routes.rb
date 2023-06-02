ce2a731e1d4c150933fde51b9dad9e40af14c7ed
Emmanuel Sanchez <fede@fede.website>
2023-06-02 17:14:26 -0600

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # action cable_server
+  mount Action.server => "/cable"
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
