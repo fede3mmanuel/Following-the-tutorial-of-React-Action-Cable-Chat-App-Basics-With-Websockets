babb2319b3a4ae341f944aa694ecdbbecfe59d15
Emmanuel Sanchez <fede@fede.website>
2023-06-03 12:55:18 -0600

    use Effect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +8,7 @@ const ws = new WebSocket("ws://localhost:3000/cable")
 function App() {
   const [messages, setMessages] = useState([]);
   const [guid, setGuid] = useState("");
+  const messagesContainer = document.createElement("messages");
 
   ws.onopen = () => {
     console.log("Connected to websocket server");
@@ -24,11 +25,32 @@ function App() {
     )
   }
 
+  useEffect(() => {
+    fetchMessages();
+  }, [])
+
+  const fetchMessages = async () => {
+    const response = await fetch("ws://localhost:3000/messages")
+    const data = await response.json();
+    setMessagesAndScrollDown(data);
+  }
+
+  const setMessagesAndScrollDown = (data) => {
+    setMessages(data);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  }
   return (
     <div className="App">
       <div className="messageHeader">
         <h1>Messages</h1>
         <p>Guid: {guid}</p>
+      </div>
+      <div className='messages' id='messages'>
+        {messages.map((message) => (
+          <div className='message' key={message.guid}>
+            <p>{message.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
